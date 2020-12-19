9386394276c00bc304a2875c44ce74bb8363adb3
bhusalashish <ashish.bhusal.16999@gmail.com>
2020-12-19 07:44:31 +0530

    Reverse string

--- day1/q10.cc
+++ day1/q10.cc
@@ -1,20 +1,15 @@
-/*
-https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps/0
-https://leetcode.com/problems/jump-game/
-*/
-
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+typedef pair<int, int> pi;
 auto speedup = [](){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	#ifndef ONLINE_JUDGE
-		freopen("input.txt", "r", stdin);
-	#endif
+	//#ifndef ONLINE_JUDGE
+	//	freopen("input.txt", "r", stdin);
+	//#endif
 	return 0;
 }();
-
 template <typename T1, typename T2>
 void print(const T1 array[], T2 size)
 {
@@ -30,13 +25,23 @@ void print(vector<T> &vec){
 }
 template<typename T>
 void print(vector<vector<T> > &vec){
-	for(auto elements : vec){
+	for(auto &elements : vec){
 		for(T &element : elements)
 			cout<<element<<" ";
 		cout<<endl;
 	}
 }
-
+template<typename T>
+void print(set<T> &set){
+	for(auto it : set)
+		cout<<it<<" ";
+	cout<<endl;
+}
+template<typename T1, typename T2>
+void print(map<T1, T2> &map){
+	for(auto it : map)
+		cout<<it.first<<" -> "<<it.second<<"\n";
+}
 template<typename T>
 void print(unordered_set<T> &uset){
 	for(auto it : uset)
@@ -48,51 +53,16 @@ void print(unordered_map<T1, T2> &umap){
 	for(auto it : umap)
 		cout<<it.first<<" -> "<<it.second<<"\n";
 }
-
-bool canJump(vector<int>& nums) {
-    int len = nums.size();
-    if(len < 2) return true;
-    int jumps[len];
-    for(int &jump : jumps)
-        jump = INT_MAX;
-    jumps[0] = 0;
-    for(int i=1; i<len; ++i){
-        for(int j=0; j<i; ++j){
-            if(jumps[j] != INT_MAX && (i <= (nums[j]+j))){
-                jumps[i] = min(jumps[i], jumps[j]+1);
-            }
-        }
-    }
-    return jumps[len-1] != INT_MAX;
-}
-
-int minJump(vector<int> &nums){
-	int len = nums.size();
-	if(len < 2) return 0;
-	int dp[len] = {0};
-	fill(dp+1, dp+len, INT_MAX);
-	for(int i=1; i<len; ++i){
-		for(int j=0; j<i; ++j){
-			if(dp[j] != INT_MAX && j+nums[i] >= i){
-				dp[i] = min(dp[i], dp[j]+1);
-			}
-		}
-	}
-	// print(dp, len);
-	// vector<vector<int>> v(10, vector<int>(10, 10));
-	print(nums);
-	return dp[len-1];
-}
-
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		vector<int> nums(n);
-		for(int &num : nums) cin>>num;
-		cout<<minJump(nums)<<endl;
+	int test_cases;
+	cin>>test_cases;
+	while(test_cases--){
+		int number_of_elements;
+		cin>>number_of_elements;
+		vector<int> nums(number_of_elements);
+		for(int &num : nums)
+			cin>>num;
+		print(nums);
+
 	}
-	return 0;
 }
